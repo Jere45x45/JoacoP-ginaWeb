036ac977255003a77cf37297d4f5b0668ad20e59
Jeremias Beyoglonian <you@example.com>
2025-08-22 15:50:48 -0300

    a

---
+++ main.js
@@ -0,0 +1,9 @@
+let nombre = document.getElementById("nombre");
+let precio = document.getElementById("precio");
+
+function agregardiv(){
+    let a = document.createElement("div");
+    //me falta decir donde va el div
+    a.textContent = nombre.textContent
+}
+
